ca863e350719d88cf27664c8dd308393d1afb094
Suraj Kaushik <kaushiksuraj1102@gmail.com>
2020-08-15 00:09:45 +0530

    Initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'contact', component:ContactMeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
