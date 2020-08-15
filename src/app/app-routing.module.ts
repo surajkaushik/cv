4303a451442b81ccfc12a74f0d9b59413dea383e
Suraj Kaushik <kaushiksuraj1102@gmail.com>
2020-08-15 15:08:54 +0530

    Added routing for education and tech. skill comp

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ContactMeComponent } from './contact-me/contact-me.component';
+import { EducationComponent } from './education/education.component';
+import { TechnicalSkillsComponent } from './technical-skills/technical-skills.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:'full'},
   {path:'home',component:HomeComponent},
-  {path:'contact', component:ContactMeComponent}
+  {path:'contact', component:ContactMeComponent},
+  {path:'education', component:EducationComponent},
+  {path:'tskill', component:TechnicalSkillsComponent}
 ];
 
 @NgModule({
